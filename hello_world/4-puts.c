568c1415c821d2b10423755236ef2f2793f2e9b8
root <fanuelp9@gmail.com>
2023-10-14 02:44:22 -0700

    dans C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/*
+ * main - Etry point
+ *
+ * return 0 (succes)
+ */
 int main(void) {
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return (0);
 }
 
