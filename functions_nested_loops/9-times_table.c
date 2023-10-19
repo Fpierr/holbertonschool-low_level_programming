b33d88de6978e585ba528fd2d257664616df90af
root <fanuelp9@gmail.com>
2023-10-19 10:52:45 -0700

    print times table 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			if (k == 0)
+			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
@@ -28,7 +28,7 @@ void times_table(void)
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
-			if (k != 9)
+			if (j < 9)
 			{
 				_putchar(',');
 				_putchar(32);
