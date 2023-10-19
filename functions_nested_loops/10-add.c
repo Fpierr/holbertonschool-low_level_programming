ba8c8d083bdad8313448ebe43169ee8a27809b09
root <fanuelp9@gmail.com>
2023-10-19 11:41:39 -0700

    Add two integer and return result

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -9,13 +9,5 @@
  */
 int add(int a, int b)
 {
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b <= 9; b++)
-		{
-			_putchar((a + b) + '0');
-		}
-		_putchar('\n');
-	}
-	return (0);
+	return (a + b);
 }
