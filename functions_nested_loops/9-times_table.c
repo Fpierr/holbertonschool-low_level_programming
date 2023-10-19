7069ab0bf56e95bc07d8b081e81a14e0e2dae0c7
root <fanuelp9@gmail.com>
2023-10-19 08:23:06 -0700

    print times table 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - print time table 
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			if (k == 0)
+			{
+				_putchar(k + '0');
+			}
+			else if (k <= 9)
+			{
+				_putchar(32);
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k %10) +'0');
+			}
+		}
+		if (k != 9)
+		{
+			_putchar(',');
+			_putchar(32);
+		}
+	}
+	_putchar('\n');
+}
