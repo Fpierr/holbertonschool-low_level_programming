d3570de628cc84119d84c21bf48019de7b6f214c
root <fanuelp9@gmail.com>
2023-10-25 05:13:18 -0700

    encodes a string into 1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,18 +9,19 @@
 char *leet(char *str)
 {
 	int i = 0;
-	int j = 0;
+	int j;
 	char lt[] = "aAeEoOtTlL";
 	char encod[] = "4433007711";
 
 	while (str[i] != '\0')
 	{
+		j = 0;
+
 		while (lt[j] != '\0')
 		{
 			if (str[i] == lt[j])
 			{
 				str[i] = encod[j];
-				break;
 			}
 			j++;
 		}
