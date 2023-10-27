0ee63a82c3195459e023616a7accd915a948d46d
root <fanuelp9@gmail.com>
2023-10-26 17:58:55 -0700

    localate a substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - locate a substring
+ * @haystack: string 1, content the substring of needle to locate
+ * @needle: string 2, is a substring in haystack
+ *
+ * Return: a pointer to the beginning of the localed substring, or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	i = 0;
+
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+
+		while (haystack[i + j] == needle[j])
+		{
+			if (needle[j + 1] == '\0')
+			{
+				return (&haystack[i]);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
