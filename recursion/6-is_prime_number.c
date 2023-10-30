4e5db30c020315af448147e8aba617370d004df4
root <fanuelp9@gmail.com>
2023-10-30 11:34:28 -0700

    function recursion

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check prime number
+ * @n: the number to check
+ *
+ * Return: 1 if the input integer is a prime number, else return 0.
+ */
+int is_prime_number(int n)
+{
+	int m = 5;
+
+	if (n <= 1)
+
+	{
+		return (0);
+	}
+	if (n >= 2 && n <= 3)
+	{
+		return (1);
+	}
+	if (n % 2 == 0 || n % 3 == 0)
+	{
+		return (0);
+	}
+	while (m * m <= n)
+	{
+		if (n % m == 0 || n % (m + 2) == 0)
+		{
+			return (0);
+		}
+		m += 6;
+	}
+	return (1);
+}
