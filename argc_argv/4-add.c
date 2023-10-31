059d4a6d855beece9ee5fd6ee11f888db272bb4f
root <fanuelp9@gmail.com>
2023-10-31 11:00:52 -0700

    Write a program that adds positive numbers - print 0 if no number, print Error and return 1 if containt symbol not digit

---
+++ argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point main
+ * @argc: the number of arguments
+ * @argv: the pointer containt an array of strings
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int j = 0;
+	int sum = 0;
+	int found = 0;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+		{
+			sum = sum + atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+
+	return (found ? 1 : 0);
+}
