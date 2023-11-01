936df4065d8a52f98a437d19123344a67f5f7598
root <fanuelp9@gmail.com>
2023-11-01 05:06:39 -0700

    create a static library

--- static_libraries/100-atoi.c
+++ static_libraries/100-atoi.c
@@ -1,8 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
 /**
  * _atoi - convert a string to an integer
@@ -12,5 +8,43 @@
  */
 int _atoi(char *s)
 {
-	return (atoi(s));
+	int i, len, sgn, digt, num, chk_dg;
+
+	i = 0;
+	len = 0;
+	sgn = 0;
+	digt = 0;
+	num = 0;
+	chk_dg = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && chk_dg == 0)
+	{
+		if (s[i] != '\0' && s[i] == '-')
+			++sgn;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digt = s[i] - '0';
+			if (sgn % 2 != 0)
+			{
+				digt = -digt;
+			}
+			num = num * 10 + digt;
+
+			chk_dg = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				break;
+			}
+			chk_dg = 0;
+		}
+		i++;
+	}
+	if (chk_dg == 0)
+		return (0);
+
+	return (num);
 }
