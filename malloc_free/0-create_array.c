d1cab3650463b3905df0363afcd4493224a3b485
root <fanuelp9@gmail.com>
2023-11-07 13:34:54 -0800

    malloc, free, calloc, realloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and initializes it with a specific char
+ * @size: the size of the array
+ * @c: the specific character to inistalize the array
+ *
+ * Return: NULL if size = 0, a pointer to the array or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *T;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	T = malloc(size * sizeof(char));
+	if (T == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		T[i] = c;
+	}
+	return (T);
+}
