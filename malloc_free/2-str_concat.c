f47dd527f5556baff88a68e7e048010d7a8426bf
root <fanuelp9@gmail.com>
2023-11-08 02:27:37 -0800

    concatenate with malloc and add $ in end of string

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,11 +16,16 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len1 = 0;
 	unsigned int len2 = 0;
 
+	if (s1 == NULL)
+		s1 = NULL;
+	if (s2 == NULL)
+		s2 = NULL;
+
 	while (s1[len1] != '\0')
 		len1++;
-
 	while (s2[len2] != '\0')
 		len2++;
+
 	T = malloc((len1 + len2 + 1) * sizeof(char));
 	if (T == NULL)
 		return (NULL);
@@ -35,6 +40,8 @@ char *str_concat(char *s1, char *s2)
 		T[i + j] = s2[j];
 	}
 	T[i + j] = '\0';
+	if (T[i + j] == '\0')
+		T[i + j] = '$';
 
 	return (T);
 }
