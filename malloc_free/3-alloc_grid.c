96e7871a162e0035fb40f58563ed9195bba51d00
root <fanuelp9@gmail.com>
2023-11-08 09:27:33 -0800

    mallocmalloc, free, calloc, realloc

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - return a pointer to a 2 dimensional array of integer
+ * @width: the larg or the number of line of the array
+ * @height: the number of line of the array
+ *
+ * Return: if width or height is negative, or if the function faillure
+ */
+int **alloc_grid(int width, int height)
+{
+	int **Arr = 0;
+	int i = 0;
+	int j = 0;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	Arr = malloc(height * sizeof(int *));
+	if (Arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		Arr[i] = malloc(width * sizeof(int));
+		if (Arr[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(Arr[j]);
+			}
+			free(Arr);
+
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			Arr[i][j] = 0;
+		}
+	}
+	return (Arr);
+}
