68f21c991dcf0622a9f0385177ffa0849d4d9367
root <fanuelp9@gmail.com>
2023-11-09 14:38:45 -0800

    value abs macro

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+/**
+ * main - Entry point
+ *
+ * Return: int
+ */
+int main(void)
+{
+	int n;
+
+	printf("%d\n", ABS(n));
+
+	return (0);
+}
