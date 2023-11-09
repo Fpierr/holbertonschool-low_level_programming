f60a5c9c4d1740b6259e3716850e402c8a8dffa1
root <fanuelp9@gmail.com>
2023-11-09 01:48:12 -0800

    malloc free

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,10 +20,8 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = NULL;
 
-	while (s1[len1] != '\0')
-		len1++;
-	while (s2[len2] != '\0')
-		len2++;
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
 	T = malloc((len1 + len2 + 1) * sizeof(char));
 	if (T == NULL)
