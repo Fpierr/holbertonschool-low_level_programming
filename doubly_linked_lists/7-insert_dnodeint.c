2eaf190b2e070794de51e3c998d056e68b0996a0
root <fanuelp9@gmail.com>
2023-11-30 09:11:39 -0800

    insert a new node in a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,28 +16,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (idx > 0)
+	while (idx > 1)
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
 		{
 			return (NULL);
 		}
-		else if (tmp->next == NULL)
-		{
-			return (add_dnodeint_end(h, n));
-		}
-		idx++;
+		idx--;
 	}
+	if (tmp->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
+	{
 		return (NULL);
-
+	}
 	new_node->n = n;
 	new_node->prev = tmp;
 	new_node->next = tmp->next;
-	(tmp->next)->prev = new_node;
-	new_node = tmp->next;
+
+	if (tmp->next != NULL)
+		tmp->next->prev = new_node;
+
+	tmp->next = new_node;
 
 	return (new_node);
 }
