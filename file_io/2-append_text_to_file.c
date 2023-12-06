36c6ec469a50ec00ce1fda0d15df1cebd8d9c4ed
root <fanuelp9@gmail.com>
2023-12-06 04:07:02 -0800

    create append file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	fd = open(filename, O_RDWR, O_APPEND);
+	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
