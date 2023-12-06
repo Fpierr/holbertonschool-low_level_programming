393eae4bcf611313120ac7743c9fd9acefbda436
root <fanuelp9@gmail.com>
2023-12-06 02:50:43 -0800

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,20 +13,25 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+
 	if (text_content != NULL)
 	{
 		while (text_content[len] != '\0')
-			len++;
-		w = write(fd, text_content, len);
-		if (w == -1)
 		{
-			close(fd);
-			return (-1);
+			len++;
 		}
+	}
+	w = write(fd, text_content, len);
+	if (w == -1)
+	{
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 
