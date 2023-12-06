043ca4106ab3965c41411a8e7a4648a4555ec590
root <fanuelp9@gmail.com>
2023-12-06 02:03:45 -0800

    create file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: the name of the file will be create
+ * @text_content: the text that content the file
+ *
+ * Return: 1 on success, or -1 on faillure or if filename is NULL
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+		w = write(fd, text_content, len);
+		if (w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+
+	return (1);
+}
