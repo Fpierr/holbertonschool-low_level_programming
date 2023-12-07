5c99eac2b29d6a877d7db0fb4706230e651d0385
root <fanuelp9@gmail.com>
2023-12-07 04:57:34 -0800

    implemente alogorithm djb2

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * hash_djb2 - implementing the djb2 algorithm
+ * @str: the string associate to the hash
+ *
+ * Return: the specifique number of hash to evite colision
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while (((c = *str++)))
+	{
+		hash = ((hash << 5) + hash) + C; /* hash * 33 + c */
+	}
+	return (hash);
+}
