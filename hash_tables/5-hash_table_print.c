175ae6feaf7e2b95b5f101fa9a68f06ae1b42102
root <fanuelp9@gmail.com>
2023-12-07 10:10:34 -0800

    print a hash table fix error of compillation

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -12,14 +12,14 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *node;
 	char *comma = "";
 
-	if (ht == NULL || ht->size == '\0')
+	if (!ht || !ht->size)
 		return;
 
 	putchar('{');
 	while (index < ht->size)
 	{
 		node = ((ht->array)[index]);
-		while (node != NULL)
+		while (node)
 		{
 			printf("%s '%s' : '%s'", comma, node->key, node->value);
 			comma = ", ";
@@ -27,6 +27,5 @@ void hash_table_print(const hash_table_t *ht)
 		}
 		index++;
 	}
-	putchar('}');
-	putchar('\n');
+	puts("}");
 }
