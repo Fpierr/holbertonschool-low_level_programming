9d6a782094f5bb42eac64b6dfe71bab5133aceda
root <fanuelp9@gmail.com>
2023-12-07 10:01:37 -0800

    print a hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table to print
+ *
+ * Return: void, if ht il NULL, don't print anything
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *node;
+	char *comma = "";
+
+	if (ht == NULL || ht->size == 0)
+		return;
+
+	putchar('{');
+	while (index < ht->size)
+	{
+		node = ((ht->array)[index]);
+		while (node != NULL)
+		{
+			printf("%s '%s' : '%s'", comma, node->key, node->value);
+			comma = ", ";
+			node = node->next;
+		}
+		index++;
+	}
+	putchar('}');
+	putchar('\n');
+}
