c3d6db560d850706ba93e307fccc6a184c41013c
root <fanuelp9@gmail.com>
2023-12-07 10:31:59 -0800

    fixing error

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -10,20 +10,27 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int index = 0;
 	hash_node_t *node;
-	char *comma = "";
+	unsigned char comma_flag = 0;
 
-	if (!ht || !ht->size)
+	if (ht == NULL)
 		return;
 
 	putchar('{');
 	while (index < ht->size)
 	{
-		node = ((ht->array)[index]);
-		while (node)
+		if (ht->array[index] != NULL)
 		{
-			printf("%s '%s' : '%s'", comma, node->key, node->value);
-			comma = ", ";
-			node = node->next;
+			if (comma_flag == 1)
+				printf(", ");
+
+			node = ht->array[index];
+			while (node != NULL)
+			{
+				printf("'%s' : '%s'", node->key, node->value);
+				node = node->next;
+				printf(", ");
+			}
+			comma_flag = 1;
 		}
 		index++;
 	}
