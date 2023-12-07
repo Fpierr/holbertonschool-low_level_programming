d534ad0f8dfcd7c7f89ebd4414591b77d553c28c
root <fanuelp9@gmail.com>
2023-12-07 10:46:57 -0800

    fix err

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -26,9 +26,9 @@ void hash_table_print(const hash_table_t *ht)
 			node = ht->array[index];
 			while (node != NULL)
 			{
-				printf("'%s' : '%s'", node->key, node->value);
+				printf("'%s': '%s'", node->key, node->value);
 				node = node->next;
-				if (node)
+				if (node != NULL)
 					printf(", ");
 			}
 			comma_flag = 1;
