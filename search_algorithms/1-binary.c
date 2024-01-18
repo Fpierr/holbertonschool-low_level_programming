f36917b7b1f67930f9a435b9d6e9b706477971a4
Fpierr <fanuelp9@gmail.com>
2024-01-18 20:34:33 +0100

    fixed error

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -7,18 +7,18 @@
  * @size: the size of the array
  * @value: the value to search
  *
- * Return: index on success (-1) on fail
+ * Return: int
  */
 int binary_search(int *array, size_t size, int value)
 {
 	size_t low = 0;
 	size_t high = size - 1;
 	size_t half = 0;
-
+	
 	if (array == NULL || size == 0)
 		return (-1);
-
 	print_array(array, low, size);
+
 	while (low < high)
 	{
 		half = (low + high) / 2;
@@ -33,7 +33,7 @@ int binary_search(int *array, size_t size, int value)
 			print_array(array, low, high + 1);
 		}
 		else
-			return (array[half]);
+			return (half);
 	}
 	return (-1);
 }
