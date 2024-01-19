d5d54f7b5a3f78d50c6b4091b9bce6f1de4e1c09
Fpierr <fanuelp9@gmail.com>
2024-01-19 10:55:43 +0100

    search algos project

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -5,6 +5,5 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_array(int *array, size_t i, size_t size);
 
 #endif /* SEARCH_ALGOS_H */
